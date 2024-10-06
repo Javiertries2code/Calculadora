31f9238d0b802ba58ec37558cd2e084801d327be
Javiertries2code <superjabo@live.com>
2024-10-06 02:53:14 +0200

    removed comments, some empty lines and set the proper preview mode for landscape

--- ComposeCalcu/app/src/main/java/com/example/composecalcu/MainActivity.kt
+++ ComposeCalcu/app/src/main/java/com/example/composecalcu/MainActivity.kt
@@ -1,31 +1,30 @@
 package com.example.composecalcu
 
-import android.annotation.SuppressLint
+
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import java.math.*
-
-
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
@@ -35,8 +34,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.composecalcu.ui.theme.ComposeCalcuTheme
-import com.example.composecalcu.ui.theme.White
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,7 +49,6 @@ class MainActivity : ComponentActivity() {
                 else -> {
                     VerticalScreenComposition()
                 }
-
         }
     }
 }
@@ -85,12 +81,12 @@ fun OperatorButton(
 
     ) {
         Button(
-            onClick = { },
+            onClick = { onOperatorClick(number)},
             colors = ButtonDefaults.buttonColors(backgroundColor),
             shape = RoundedCornerShape(12.dp),
             modifier = Modifier.padding(8.dp).width(70.dp).height(50.dp)
         ) {
-            Text(text = number.toString(), fontSize = 14.sp, color = Color.Black)
+            Text(text = number, fontSize = 14.sp, color = Color.Black)
         }
     }
 
@@ -108,19 +104,19 @@ fun NumberButton(
         shape = RoundedCornerShape(12.dp),
         modifier = Modifier.padding(8.dp).width(75.dp).height(50.dp)
     ) {
-        Text(text = number.toString(), fontSize = 18.sp, color = Color.Black)
+        Text(text = number, fontSize = 18.sp, color = Color.Black)
     }
 }
 
-    @Preview(showBackground = true)
+   @Preview(showBackground = true, showSystemUi = true,
+   device = "spec:width=411dp,height=891dp,orientation=landscape")
     @Composable
     fun LandscapeScreenComposition() {
         var numberDisplay by rememberSaveable { mutableStateOf("0") }
         var sign: String
-        fun OperatorButtonClick(operator: String) {
-            if (operator == "CE")
-                numberDisplay = "0"
-            if (operator == "C")
+        fun operatorButtonClick(operator: String) {
+
+            if (operator == "C" || operator == "ON")
                 numberDisplay = "0"
             else if (numberDisplay == "0" && operator !=  "+/-")
                 numberDisplay = operator
@@ -129,7 +125,7 @@ fun NumberButton(
                     numberDisplay = "0"
                 else {
                     if (numberDisplay[0] != '-') {
-                        sign = "-" + numberDisplay
+                        sign = "-$numberDisplay"
                         numberDisplay = sign
                     } else {
                         sign = numberDisplay.substring(1)
@@ -141,7 +137,7 @@ fun NumberButton(
 
         }
 
-        fun NumberButtonClick(number: String) {
+        fun numberButtonClick(number: String) {
             if (numberDisplay == "0")
                 numberDisplay = number
             else
@@ -180,40 +176,11 @@ fun NumberButton(
                 modifier = Modifier.fillMaxWidth()
             )
             {
-                NumberButton(number = "7", onNumberClick = ::NumberButtonClick)
-                NumberButton(number = "8", onNumberClick = ::NumberButtonClick)
-                NumberButton(number = "9", onNumberClick = ::NumberButtonClick)
-                OperatorButton(operator = "+", onOperatorClick = ::OperatorButtonClick)
-                OperatorButton(operator = "-", onOperatorClick = ::OperatorButtonClick)
-
-            }
-
-            // LineaNum(4,5,6, "-")
-            Row(
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier.fillMaxWidth()
-            )
-            {
-                NumberButton(number = "4", onNumberClick = ::NumberButtonClick)
-                NumberButton(number = "5", onNumberClick = ::NumberButtonClick)
-                NumberButton(number = "6", onNumberClick = ::NumberButtonClick)
-                OperatorButton(operator = "+-", onOperatorClick = ::OperatorButtonClick)
-                OperatorButton(operator = "%", onOperatorClick = ::OperatorButtonClick)
-
-
-
-            }
-            // LineaNum(1,2,3, "/")
-            Row(
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier.fillMaxWidth()
-            )
-            {
-                NumberButton(number = "1", onNumberClick = ::NumberButtonClick)
-                NumberButton(number = "2", onNumberClick = ::NumberButtonClick)
-                NumberButton(number = "3", onNumberClick = ::NumberButtonClick)
-                OperatorButton(operator = "/", onOperatorClick = ::OperatorButtonClick)
-                OperatorButton(operator = "X", onOperatorClick = ::OperatorButtonClick)
+                NumberButton(number = "7", onNumberClick = ::numberButtonClick)
+                NumberButton(number = "8", onNumberClick = ::numberButtonClick)
+                NumberButton(number = "9", onNumberClick = ::numberButtonClick)
+                OperatorButton(operator = "+", onOperatorClick = ::operatorButtonClick)
+                OperatorButton(operator = "-", onOperatorClick = ::operatorButtonClick)
 
             }
 
@@ -222,20 +189,40 @@ fun NumberButton(
                 modifier = Modifier.fillMaxWidth()
             )
             {
-                NumberButton(number = "0", onNumberClick = ::NumberButtonClick)
-                NumberButton(number = ".", onNumberClick = ::NumberButtonClick)
-               // NumberButton(number = "00", onNumberClick = ::NumberButtonClick)
-                OperatorButton(operator = "=", onOperatorClick = ::OperatorButtonClick)
-                OperatorButton(operator = "C", onOperatorClick = ::OperatorButtonClick)
-                PowerButton(number = "ON", onOperatorClick = ::OperatorButtonClick)
+                NumberButton(number = "4", onNumberClick = ::numberButtonClick)
+                NumberButton(number = "5", onNumberClick = ::numberButtonClick)
+                NumberButton(number = "6", onNumberClick = ::numberButtonClick)
+                OperatorButton(operator = "+-", onOperatorClick = ::operatorButtonClick)
+                OperatorButton(operator = "%", onOperatorClick = ::operatorButtonClick)
 
-                // OperatorButton(operator = "\u221A", onOperatorClick = ::OperatorButtonClick)
 
+
+            }
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth()
+            )
+            {
+                NumberButton(number = "1", onNumberClick = ::numberButtonClick)
+                NumberButton(number = "2", onNumberClick = ::numberButtonClick)
+                NumberButton(number = "3", onNumberClick = ::numberButtonClick)
+                OperatorButton(operator = "/", onOperatorClick = ::operatorButtonClick)
+                OperatorButton(operator = "X", onOperatorClick = ::operatorButtonClick)
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth()
+            )
+            {
+                NumberButton(number = "0", onNumberClick = ::numberButtonClick)
+                NumberButton(number = ".", onNumberClick = ::numberButtonClick)
+                OperatorButton(operator = "=", onOperatorClick = ::operatorButtonClick)
+                OperatorButton(operator = "C", onOperatorClick = ::operatorButtonClick)
+                PowerButton(number = "ON", onOperatorClick = ::operatorButtonClick)
             }
 
         }
-
-
     }
 
 
@@ -244,7 +231,7 @@ fun NumberButton(
 fun VerticalScreenComposition() {
     var numberDisplay by rememberSaveable { mutableStateOf("0") }
     var sign: String
-    fun OperatorButtonClick(operator: String) {
+    fun operatorButtonClick(operator: String) {
         if (operator == "ON")
             numberDisplay = "0"
         else if (operator == "CE")
@@ -270,7 +257,7 @@ fun VerticalScreenComposition() {
 
     }
 
-    fun NumberButtonClick(number: String) {
+    fun numberButtonClick(number: String) {
         if (numberDisplay == "0")
             numberDisplay = number
         else
@@ -301,54 +288,42 @@ fun VerticalScreenComposition() {
                 ),
             )
         }
-        //LineaOp("C", "%","/", "X")
+
         Row(
             horizontalArrangement = Arrangement.SpaceEvenly,
             modifier = Modifier.fillMaxWidth()
         )
         {
-            PowerButton(number = "ON", onOperatorClick = ::OperatorButtonClick)
-            OperatorButton(operator = "C", onOperatorClick = ::OperatorButtonClick)
-            OperatorButton(operator = "/", onOperatorClick = ::OperatorButtonClick)
-            OperatorButton(operator = "X", onOperatorClick = ::OperatorButtonClick)
+            PowerButton(number = "ON", onOperatorClick = ::operatorButtonClick)
+            OperatorButton(operator = "C", onOperatorClick = ::operatorButtonClick)
+            OperatorButton(operator = "/", onOperatorClick = ::operatorButtonClick)
+            OperatorButton(operator = "X", onOperatorClick = ::operatorButtonClick)
 
         }
 
-        //   LineaNum(7,8,9, "+")
+
         Row(
             horizontalArrangement = Arrangement.SpaceEvenly,
             modifier = Modifier.fillMaxWidth()
         )
         {
-            NumberButton(number = "7", onNumberClick = ::NumberButtonClick)
-            NumberButton(number = "8", onNumberClick = ::NumberButtonClick)
-            NumberButton(number = "9", onNumberClick = ::NumberButtonClick)
-            OperatorButton(operator = "+", onOperatorClick = ::OperatorButtonClick)
+            NumberButton(number = "7", onNumberClick = ::numberButtonClick)
+            NumberButton(number = "8", onNumberClick = ::numberButtonClick)
+            NumberButton(number = "9", onNumberClick = ::numberButtonClick)
+            OperatorButton(operator = "+", onOperatorClick = ::operatorButtonClick)
 
         }
 
-        // LineaNum(4,5,6, "-")
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier.fillMaxWidth()
-        )
-        {
-            NumberButton(number = "4", onNumberClick = ::NumberButtonClick)
-            NumberButton(number = "5", onNumberClick = ::NumberButtonClick)
-            NumberButton(number = "6", onNumberClick = ::NumberButtonClick)
-            OperatorButton(operator = "-", onOperatorClick = ::OperatorButtonClick)
 
-        }
-        // LineaNum(1,2,3, "/")
         Row(
             horizontalArrangement = Arrangement.SpaceEvenly,
             modifier = Modifier.fillMaxWidth()
         )
         {
-            NumberButton(number = "1", onNumberClick = ::NumberButtonClick)
-            NumberButton(number = "2", onNumberClick = ::NumberButtonClick)
-            NumberButton(number = "3", onNumberClick = ::NumberButtonClick)
-            OperatorButton(operator = "%", onOperatorClick = ::OperatorButtonClick)
+            NumberButton(number = "4", onNumberClick = ::numberButtonClick)
+            NumberButton(number = "5", onNumberClick = ::numberButtonClick)
+            NumberButton(number = "6", onNumberClick = ::numberButtonClick)
+            OperatorButton(operator = "-", onOperatorClick = ::operatorButtonClick)
 
         }
 
@@ -357,15 +332,25 @@ fun VerticalScreenComposition() {
             modifier = Modifier.fillMaxWidth()
         )
         {
-            NumberButton(number = "0", onNumberClick = ::NumberButtonClick)
-            NumberButton(number = ".", onNumberClick = ::NumberButtonClick)
-            OperatorButton(operator = "+-", onOperatorClick = ::OperatorButtonClick)
-            OperatorButton(operator = "=", onOperatorClick = ::OperatorButtonClick)
+            NumberButton(number = "1", onNumberClick = ::numberButtonClick)
+            NumberButton(number = "2", onNumberClick = ::numberButtonClick)
+            NumberButton(number = "3", onNumberClick = ::numberButtonClick)
+            OperatorButton(operator = "%", onOperatorClick = ::operatorButtonClick)
+
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth()
+        )
+        {
+            NumberButton(number = "0", onNumberClick = ::numberButtonClick)
+            NumberButton(number = ".", onNumberClick = ::numberButtonClick)
+            OperatorButton(operator = "+-", onOperatorClick = ::operatorButtonClick)
+            OperatorButton(operator = "=", onOperatorClick = ::operatorButtonClick)
 
         }
     }
-
-
 }}
 
 
